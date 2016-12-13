a28b1d8cc57b4ccaf9fb59a41906da4683b4ecac
user <user@user-PC>
2016-12-13 21:21:10 +0530

    This is my first commit.

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ and open the template in the editor.
         <?php
             echo 'Netbeans proj';
             echo 'New Line added';
+            echo 'Another line added';
         ?>
     </body>
 </html>
